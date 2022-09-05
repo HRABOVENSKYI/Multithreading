d168b9ff4518b1000fb61d20f452339ca05d7eb6
Teodor Hrabovenskyi <teodor.hrabovenskyi@dataart.com>
2022-09-05 15:59:12 +0300

    Resource sharing. Logger. Problem about resource sharing

--- src/ua/lviv/dataart/App.java
+++ src/ua/lviv/dataart/App.java
@@ -1,8 +1,80 @@
 package ua.lviv.dataart;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class App {
 
-    public static void main(String[] args) {
+    private static final Logger logger = Logger.getLogger("AppLogger");
 
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = Counter.getInstance();
+        IncrementalThread incrementalThread = new IncrementalThread(counter);
+        DecrementalThread decrementalThread = new DecrementalThread(counter);
+
+        incrementalThread.start();
+        decrementalThread.start();
+        incrementalThread.join();
+        decrementalThread.join();
+        logger.log(Level.INFO, "The end count is: {0}", counter.getCount()); // result is not 0 !!!
+    }
+
+    private static class Counter {
+        private static Counter counter;
+        private static final int INITIAL_COUNT = 0;
+        private int count;
+
+        private Counter(int count) {
+            this.count = count;
+        }
+
+        public static Counter getInstance() {
+            if (counter == null) {
+                counter = new Counter(INITIAL_COUNT);
+            }
+            return counter;
+        }
+
+        public void decrement() {
+            this.count--;
+        }
+
+        public void increment() {
+            this.count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    private static class IncrementalThread extends Thread {
+        private Counter counter;
+
+        public IncrementalThread(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        }
+    }
+
+    private static class DecrementalThread extends Thread {
+        private Counter counter;
+
+        public DecrementalThread(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                counter.decrement();
+            }
+        }
     }
 }
