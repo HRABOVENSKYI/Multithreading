6f1c293e585300f09e7076168b1c2244627cbfdf
thrabovenskyi <Teodor.Hrabovenskyi@dataart.com>
2023-08-15 19:05:09 +0300

    Implemented the game

--- src/ua/lviv/dataart/Main.java
+++ src/ua/lviv/dataart/Main.java
@@ -6,54 +6,100 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    private static final int MAX_PASSWORD = 9999;
 
     public static void main(String[] args) throws InterruptedException {
-        List<Long> inputNumbers = Arrays.asList(10000000l, 3445l, 234l, 3455l, 2243l, 23l, 459l);
-        List<FactorialThread> threads = new ArrayList<>();
-        inputNumbers.forEach(inputNumber -> threads.add(new FactorialThread(inputNumber)));
+        Vault vault = new Vault(1064);
+        List<Thread> threads = new ArrayList<>();
+        threads.add(new AscendingHackerThread(vault));
+        threads.add(new DescendingHackerThread(vault));
+        threads.add(new PoliceThread());
         threads.forEach(Thread::start);
-        for (Thread thread : threads) {
-            thread.join(2000);
-        }
-        for (int i = 0; i < inputNumbers.size(); i++) {
-            FactorialThread factorialThread = threads.get(i);
-            if (factorialThread.isFinished()) {
-                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
-            } else {
-                System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress");
-            }
+    }
+
+    private static void sleepHandled(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static class FactorialThread extends Thread {
-        private final long inputNumber;
-        private BigInteger result = BigInteger.ZERO;
-        private boolean isFinished = false;
+    private static class Vault {
+        private int password;
 
-        public FactorialThread(long inputNumber) {
-            this.inputNumber = inputNumber;
+        public Vault(int password) {
+            this.password = password;
+        }
+
+        public void setPassword(int password) {
+            this.password = password;
+        }
+
+        public boolean isPasswordCorrect(int inputPss) {
+            sleepHandled(5);
+            return this.password == inputPss;
+        }
+    }
+
+    private static class HackerThread extends Thread {
+        protected Vault vault;
+
+        public HackerThread(Vault vault) {
+            this.vault = vault;
+            this.setName(this.getClass().getSimpleName());
+            this.setPriority(MAX_PRIORITY);
+        }
+
+        @Override
+        public void start() {
+            System.out.println("Hacker " + this.getName() + " started guessing vault's password");
+            super.start();
+        }
+    }
+
+    private static class AscendingHackerThread extends HackerThread {
+        public AscendingHackerThread(Vault vault) {
+            super(vault);
         }
 
         @Override
         public void run() {
-            this.result = factorial(inputNumber);
-            this.isFinished = true;
-        }
-
-        public BigInteger factorial(long n) {
-            BigInteger tempResult = BigInteger.ONE;
-            for (long i = n; i > 0; i--) {
-                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
+            for (int i = 0; i < MAX_PASSWORD; i++) {
+                if (vault.isPasswordCorrect(i)) {
+                    System.out.println(this.getName() + " stole all money");
+                    System.exit(0);
+                }
             }
-            return tempResult;
+        }
+    }
+
+    private static class DescendingHackerThread extends HackerThread {
+        public DescendingHackerThread(Vault vault) {
+            super(vault);
         }
 
-        public boolean isFinished() {
-            return isFinished;
+        @Override
+        public void run() {
+            for (int i = MAX_PASSWORD; i > 0; i--) {
+                if (vault.isPasswordCorrect(i)) {
+                    System.out.println(this.getName() + " stole all money");
+                    System.exit(0);
+                }
+            }
         }
+    }
 
-        public BigInteger getResult() {
-            return result;
+    private static class PoliceThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Policeman: I'm counting to 10 and I'll kick your asses, hackers");
+            for (int i = 10; i > 0; i--) {
+                sleepHandled(1000);
+                System.out.println(i);
+            }
+            System.out.println("You have lost, hackers");
+            System.exit(0);
         }
     }
 }
