a7fd6e26761ddc5024f03ea9c309cf60026e5b98
thrabovenskyi <Teodor.Hrabovenskyi@dataart.com>
2023-08-25 20:01:19 +0300

    Wrote code

--- src/ua/lviv/dataart/Main.java
+++ src/ua/lviv/dataart/Main.java
@@ -1,7 +1,25 @@
 package ua.lviv.dataart;
 
-public class Main {
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Main implements Runnable {
+    int counter = 1;
 
     public static void main(String[] args) {
+        Main main = new Main();
+        Executor executor = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executor.execute(main);
+        }
+
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            System.out.println(counter);
+            counter++;
+        }
     }
 }
