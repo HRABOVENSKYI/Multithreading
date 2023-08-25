4dc0fb8c67727748ba3b0b19dda3c0bcc17a8754
thrabovenskyi <Teodor.Hrabovenskyi@dataart.com>
2023-08-25 21:13:21 +0300

    Wrote code

--- src/ua/lviv/dataart/Main.java
+++ src/ua/lviv/dataart/Main.java
@@ -2,6 +2,39 @@ package ua.lviv.dataart;
 
 public class Main {
 
+    private static class SharedClass {
+        // if we add volatile keyword to x and y, they will be executed in order they are written in methods, and therefore,
+        // no race condition will happen
+        private int x;
+        private int y;
+
+        public void increment() {
+            x++;
+            y++;
+        }
+
+        public void checkDataRace() {
+            // if we add volatile keyword to x and y, they will be executed in order they are written in methods, and therefore,
+            // no race condition will happen
+            if (y > x) {
+                System.out.printf("Data race have happened. x=%d, y=%d%n", x, y);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        SharedClass sharedClass = new SharedClass();
+        Thread incrementingThread = new Thread(() -> {
+            while(true) {
+                sharedClass.increment();
+            }
+        });
+        Thread checkDataRaceThread = new Thread(() -> {
+            while(true) {
+                sharedClass.checkDataRace();
+            }
+        });
+        incrementingThread.start();
+        checkDataRaceThread.start();
     }
 }
